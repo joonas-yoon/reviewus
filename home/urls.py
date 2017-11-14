4c7946a3b0365eea3d466eb24477cca21066a75a
Ubuntu <ubuntu@ip-172-31-10-133.ap-northeast-2.compute.internal>
2017-11-14 03:01:55 +0000

    Tutorial 03-1 - Start view with HTML

--- home/urls.py
+++ home/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url
 
-from . import views
+from home import views
 
 urlpatterns = [
   url(r'^$', views.index, name='index'),
+  url(r'^(?P<echo_msg>\w+)/$', views.echo, name='echo'),
 ]
--- home/views.py
+++ home/views.py
@@ -1,8 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from datetime import datetime, timezone, timedelta
 
 def index(request):
-    return HttpResponse("<h1>Hello, world.</h1>")
+  tz = 9
+  now = datetime.time(datetime.now(timezone(timedelta(hours=tz))))
+  current_time = "%02d:%02d:%02d</h1>" % (now.hour, now.minute, now.second)
+  context = {
+    'current_time': current_time,
+    'now': now
+  }
 
+  return render(request, 'home/index.html', context)
+
+def echo(request, echo_msg):
+  res = "<h1>Hello, %s</h1>"
+  return HttpResponse(res % echo_msg)
 
