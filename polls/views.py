63ce4d9b3ad7139ef9a7b0c450d686b7e8669eb3
Aws Instance <ubuntu@ip-172-31-10-133.ap-northeast-2.compute.internal>
2017-11-14 14:21:02 +0000

    Add some codes for Poll on views, urls with models

--- polls/views.py
+++ polls/views.py
@@ -1,17 +1,42 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponseRedirect, HttpResponse
+from django.urls import reverse
 
-from django.http import HttpResponse
+from .models import Choice, Question
 
 
+# /polls/
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+  latest_question_list = Question.objects.order_by('-pub_date')[:5]
+  context = {
+    'latest_question_list': latest_question_list
+  }
+  return render(request, 'polls/index.html', context)
 
-def result(request, question_id):
-    return HttpResponse("Result with #" + question_id)
-
-def vote(request, question_id):
-    return HttpResponse("Vote with #" + question_id)
-
+# /polls/1/
 def detail(request, question_id):
-    return HttpResponse("Detail with #" + question_id)
+  question = get_object_or_404(Question, pk=question_id)
+  return render(request, 'polls/detail.html', {'question': question})
+
+# /polls/1/results
+def results(request, question_id):
+  question = get_object_or_404(Question, pk=question_id)
+  return render(request, 'polls/results.html', {
+    'question': question
+  })
+
+# /polls/1/vote
+def vote(request, question_id):
+  question = get_object_or_404(Question, pk=question_id)
+  try:
+    selected_choice = question.choice_set.get(pk=request.POST['choice'])
+  except (KeyError, Choice.DoesNotExist):
+    return render(request, 'polls/detail.html', {
+      'question': question,
+      'error_message': "you didn't select a choice.",
+    })
+  else:
+    selected_choice.votes += 1
+    selected_choice.save()
+    return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
 
