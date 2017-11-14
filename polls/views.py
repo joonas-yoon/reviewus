4c989f00c6cfc087e5948298de2b9d34e6a4e839
Ubuntu <ubuntu@ip-172-31-10-133.ap-northeast-2.compute.internal>
2017-11-14 04:17:33 +0000

    Tutorial 03-2 - Using Template

--- polls/views.py
+++ polls/views.py
@@ -6,3 +6,12 @@ from django.http import HttpResponse
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
 
+def result(request, question_id):
+    return HttpResponse("Result with #" + question_id)
+
+def vote(request, question_id):
+    return HttpResponse("Vote with #" + question_id)
+
+def detail(request, question_id):
+    return HttpResponse("Detail with #" + question_id)
+
